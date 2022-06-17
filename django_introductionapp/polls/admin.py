17f9581e7a6c2d0ac4bceeaba0e524876750bcc9
Guillermo Cacho <guillermo.cacho.v@gmail.com>
2022-06-17 13:26:42 -0600

    Administrador completado - Se agregaron nuevas funciones y filtros para busquedas

--- django_introductionapp/polls/admin.py
+++ django_introductionapp/polls/admin.py
@@ -8,7 +8,10 @@ class ChoiceInline(admin.StackedInline): # Para agregar la opción de poner resp
     extra = 3       # Habrá 3 espacios para llenar
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ["pub_date", "question_text"] #Define el orden de los campos en el admin
+    fields = ["pub_date", "question_text"] #D efine el orden de los campos en el admin
     inlines = [ChoiceInline]
+    list_display = ("question_text", "pub_date", "was_published_recently") # Añade campos en la seccion de preguntas en el admin
+    list_filter = ["pub_date"] # Filtro sencillo por fecha de publicación
+    search_fields = ["question_text"] # Crea un cuadro de búsqueda.
 
-admin.site.register(Question, QuestionAdmin) #Hereda los fields de QuestionAdmin
+admin.site.register(Question, QuestionAdmin) #H ereda los fields de QuestionAdmin
