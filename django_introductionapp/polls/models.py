ec9b3eba0a1060292dc5066abe53c452e762ffd7
Guillermo Cacho <guillermo.cacho.v@gmail.com>
2022-06-17 11:35:26 -0600

    Typo de errores corregido

--- django_introductionapp/polls/models.py
+++ django_introductionapp/polls/models.py
@@ -9,11 +9,14 @@ class Question(models.Model): #crea el modelo con el import models
     question_text = models.CharField(max_length=200) #lo convierte en varchar
     pub_date = models.DateTimeField("date published")
 
+
     def __str__(self): #Método dunder string
         return self.question_text #Que muestre el valor del texto
 
+
     def was_published_recently(self):
-        return self.pub_date >= timezone.timezone.now() - datetime.timedelta(days=1) #Le resta al tiempo actual un día. necesita imports de date y datetime / Si la pregunta es más reciente a 1 día entonces True
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1) #Le resta al tiempo actual un día. necesita imports de date y datetime / Si la pregunta es más reciente a 1 día entonces True
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE) #Es la llave foranea de Question y eliminará toda la linea en caso de necesitarse en forma de cascada
