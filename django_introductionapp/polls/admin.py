b8d75d19ccec8be420da74c537026bd72cdf3c9a
Guillermo Cacho <guillermo.cacho.v@gmail.com>
2022-06-17 13:16:04 -0600

    Admin mejorado, acomodo de contenido y agregar respuestas a las preguntas

--- django_introductionapp/polls/admin.py
+++ django_introductionapp/polls/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
 # Register your models here.
-admin.site.register(Question)
+
+class ChoiceInline(admin.StackedInline): # Para agregar la opción de poner respuestas en las preguntas
+    model = Choice
+    extra = 3       # Habrá 3 espacios para llenar
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"] #Define el orden de los campos en el admin
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin) #Hereda los fields de QuestionAdmin
